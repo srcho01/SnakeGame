13c00535a0461849476401faf279a747b1724578
srcho01 <seorim5306@gmail.com>
2022-06-01 22:39:37 +0900

    Modified map - 변경이 필요한 칸을 각 해당 함수로 map 데이터를 변경했던 것에서 changeMap 함수를 public으로 변경하여 changeMap으로 변경하도록 함 (맵 데이터를 변경해야할 것에 비해 함수의 종류가 너무 많아 불편) - Gate는 벽 위에서만 생성이 가능한데, 이를 랜덤으로 생성할 시 벽의 수가 상대적으로 적어 Gate 생성에 오래 걸림 -> setWall 함수를 통해 변경 가능한 벽의 좌표를 리스트로 생성하는 함수 제작

--- map.cpp
+++ map.cpp
@@ -12,29 +12,16 @@ void Map::setMap(int stage) {
     else if(stage == 2) map = stage2;
     else if(stage == 3) map = stage3;
     else if(stage == 4) map = stage4;
+
+    setWall(currentStage);
 }
 
-int Map::getPosition(int x, int y) {return map[x][y] - '0';} 
-
-void Map::makeFree(int x, int y) {changeMap(x, y, 0);}
-
-void Map::makeHead(int x, int y) {
-    if(getPosition(x, y) == 0) changeMap(x, y, 3);
-}
-
-void Map::makeBody(int x, int y) {
-    if(getPosition(x, y) == 0) changeMap(x, y, 4);
-}
-
-void Map::makeGrowth(int x, int y) {
-    if(getPosition(x, y) == 0) changeMap(x, y, 5);
-}
-
-void Map::makePoison(int x, int y) {
-    if(getPosition(x, y) == 0) changeMap(x, y, 6);
-}
-
-void Map::makeGate(int x1, int y1, int x2, int y2) {
-    if(getPosition(x1, y1) == 1) changeMap(x1, y1, 7); 
-    if(getPosition(x2, y2) == 1) changeMap(x2, y2, 7);
+int Map::setWall(int stage) {
+    for(int i=0; i<25; i++)
+        for(int j=0; j<25; j++)
+            if(getPosition(i,j) == 1 || getPosition(i,j) == 8) {
+                wall.push_back(i);
+                wall.push_back(j);
+            }
+    return wall.size()/2;
 }
--- map.h
+++ map.h
@@ -2,24 +2,23 @@
 #define _MAP_H_
 
 #include <string>
+#include <vector>
 #include "mapData.h"
 
 class Map {
     std::string* map;
-    int currentStage;
-    void changeMap(int x, int y, int n) {map[x][y] = n + '0';}
     
-public:
+    int currentStage;
+    
+public:    
+    std::vector<int> wall;
+
     Map(int stage=1);
-    ~Map();
     void setMap(int stage=1);
-    int getPosition(int x, int y); 
+    int setWall(int stage);
+    int getPosition(int x, int y) {return map[x][y] - '0';}
     int getCurrentStage() {return currentStage;}
-    void makeFree(int x, int y);
-    void makeHead(int x, int y);
-    void makeBody(int x, int y);
-    void makeGrowth(int x, int y);
-    void makePoison(int x, int y);
-    void makeGate(int x1, int y1, int x2, int y2);
+    void changeMap(int x, int y, int n) {map[x][y] = n + '0';}
 };
+
 #endif
