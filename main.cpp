874d238b9e4c81a4074354ffa839f5623cdd09c4
srcho01 <seorim5306@gmail.com>
2022-06-01 22:24:53 +0900

    Modified main.cpp 한글 주석 인코딩 다시

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include "map.h"
+Map *gameMap = new Map;
+
 #include "gameUI.h"
+#include "PlayGame.h"
+#include <iostream>
+using namespace std;
 
 int main() {
     GameUI gameUI;
-    gameUI.PrintGame();
+    PlayGame playgame;
+    while(playgame.playing()) {
+        gameUI.update(1000000); // ¥‹¿ß : microseconds (1s = 1000000ms)
+    }
 }
