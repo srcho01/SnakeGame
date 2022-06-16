0181f7a7bb17abbc09bab7855ad55a75382c7b91
srcho01 <seorim5306@gmail.com>
2022-06-16 15:49:57 +0900

    Rearrange Array Size

--- PlayGame.h
+++ PlayGame.h
@@ -10,7 +10,7 @@
 extern Map *gameMap;
 class PlayGame {
 private:
-    int mission[4][5] =  // 미션 달성을 위한 몸 최소 길이, 획득해야 할 Growth Item 수, 획득해야 할 Poison Item 수, Gate 사용 횟수
+    int mission[4][4] =  // 미션 달성을 위한 몸 최소 길이, 획득해야 할 Growth Item 수, 획득해야 할 Poison Item 수, Gate 사용 횟수
     {
         {4, 1, 0, 0}, // stage 1
         {4, 1, 1, 1}, // stage 2
